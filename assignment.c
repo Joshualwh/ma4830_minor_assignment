26f138fb7849eb5dba8e3f19e80f6b20f0f5a96e
Joshualwh <joshualing0711@gmail.com>
2021-09-27 21:28:58 +0800

    done

--- assignment.c
+++ assignment.c
@@ -1,38 +1,77 @@
 #include <stdio.h>
+#include <string.h>
 
 /* User Input to choose which one to run and calculate */
-int user_input (shape_input) {
-    long shape_input ;
+void user_input (char *shape_chosen) {
 
-    printf("Enter type of shape you want : ");
-    scanf("%d", &a);
+   const char square[] = "square";
+   const char circle[] = "circle";
+   const char rectangle[] = "rectangle";
+   const char cube[] = "cube";
+   const char sphere[] = "sphere";
+   const char cone[] = "cone";
+   const char cuboid[] = "cuboid";
 
-    area = a*a;
-    square_perimeter = 4*a;
+   if (strncmp(shape_chosen, square, 7) == 0){
+      square();
+   }
+   else if (strncmp(shape_chosen, circle, 7) == 0){
+      circle();
+   }
+   else if (strncmp(shape_chosen, rectangle, 10) == 0){
+      rectangle();
+   }
+   else if (strncmp(shape_chosen, cube, 5) == 0){
+      cube();
+   }
+   else if (strncmp(shape_chosen, sphere, 7) == 0){
+      sphere();
+   }
+   else if (strncmp(shape_chosen, cone, 5) == 0){
+      cone();
+   }
+   else if (strncmp(shape_chosen, cuboid, 7) == 0){
+      cuboid():
+   }
+   else{
+      printf ("Error! No shape found!");
+   }
 
-    printf("%d \n", area);
-    printf("%d \n", square_perimeter);
-}
+   // const char shape_choices[7][10] = {"square ", "circle ", "rectangle ", "cube ", "sphere ", "cone ", "cuboid "};
 
-int square(a){
-    /* 2D Square */
-    int a, area, square_perimeter;
+   // int n = 0;
+   // while (n < 8){
+   //    printf ("%s", shape_choices[n]);
+   //    n++;
+   // }
 
-    printf("Please Enter any integer Value : ");
-    scanf("%d", &a);
+   // int checking_status1, checking_status2;
 
-    area = a*a;
-    square_perimeter = 4*a;
+   // printf("%s\n", shape_chosen);
+   // printf("%s\n", cube);
+   // checking_status1 = strncmp(shape_chosen, cube, 5);
+   // checking_status2 = strncmp(shape_chosen, square, 7);
 
-    printf("%d \n", area);
-    printf("%d \n", square_perimeter);
+   // printf("%d, %d\n", checking_status1, checking_status2);
+
+   // while (checking_status != 0){
+   //    checking_status = strncmp(shape_chosen, cube, 4);
+   // }
+   // if (strncmp(shape_chosen, cube, 4) == 0){
+   //    printf("%s", shape_chosen);
+   // }
+
+   //  printf("%d \n", area);
+   //  printf("%d \n", square_perimeter);
 }
 
 
 int main () {
 
-    char b
-    printf("Please Enter Choice for shape : ");
-    scanf("%d", &b);
-    square()
+   char shape_input[10];
+   printf("Enter choice for shape : ");
+   scanf("%s", shape_input);
+   // printf("%s\n", shape_input);
+   user_input(shape_input);
+   // if shape_input 
 }
