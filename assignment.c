661f67b5864147cc082fe421f83aea6322402bb9
JC Chia <jingcheng19986630@outlook.com>
2021-09-27 21:17:33 +0800

    Done output

--- assignment.c
+++ assignment.c
@@ -1,23 +1,46 @@
 #include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+#include <string.h>
 
-int square(a){
+struct Shape{
+    char shape_input[20];
+    float circumference;
+    float area;
+    float surface_area;
+    float volume;
+    bool two_dimension;
+    };
+
+
+void square(struct Shape *shape){
     /* 2D Square */
-    int a, area, square_perimeter;
+    printf("Please Enter any integer Value(m) : ");
+    float a;
+    scanf("%f", &a);
+    strcpy(shape->shape_input, "square");
+    shape->area = a*a;
+    shape->circumference = 4*a;
+    shape->two_dimension = true;
+}
 
-    printf("Please Enter any integer Value : ");
-    scanf("%d", &a);
-
-    area = a*a;
-    square_perimeter = 4*a;
-
-    printf("%d \n", area);
-    printf("%d \n", square_perimeter);
+void output(struct Shape *shape){
+    if (shape->two_dimension){
+        printf("Types of 2D object: %s \n", shape->shape_input);
+        printf("Area: %f m^2 \n", shape->area);
+        printf("Circumference: %f m \n", shape->circumference);
+    }
+    else{
+        printf("Types of 3D object: %s \n", shape->shape_input);
+        printf("Circumference: %f m \n", shape->circumference);
+        printf("Surface area: %f m^2 \n", shape->surface_area);
+        printf("Volume: %f m^3 \n", shape->volume);
+    }
 }
 
 int main () {
-
-    char b
-    printf("Please Enter Choice for shape : ");
-    scanf("%d", &b);
-    square()
+    struct Shape shape;
+    printf("ahah \n");
+    square(&shape);
+    output(&shape);
 }
